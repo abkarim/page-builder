d48033aa9ac307ba7eba0f4982a9d976eca4e7ab
abkarim <akskarim17@gmail.com>
2022-12-07 04:43:47 +0600

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{jsx, js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
