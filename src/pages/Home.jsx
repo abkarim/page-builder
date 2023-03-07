d2a5d8bd2bc354d74ab62f3b44b020af65ee0558
unknown <akskarim17@gmail.com>
2023-03-07 18:45:46 +0600

    pages init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import NavContainer from '../components/NavContainer'
+
+export default function Home() {
+    return (
+        <main className="p-2 flex gap-2">
+            <NavContainer icon="&#43;" text="Create new page" />
+        </main>
+    )
+}
