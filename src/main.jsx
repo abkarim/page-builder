9345cdb222cb42d82d7269e680eb80d1e84292a8
unknown <akskarim17@gmail.com>
2023-03-07 18:45:04 +0600

    React router dom added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './App.css'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </BrowserRouter>
+)
