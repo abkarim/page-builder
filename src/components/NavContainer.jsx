7092a4f9dcc01dace8c83738c227415862c56c3e
unknown <akskarim17@gmail.com>
2023-03-09 18:32:47 +0600

    Nav updated

--- src/components/NavContainer.jsx
+++ src/components/NavContainer.jsx
@@ -1,22 +1,26 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export default function NavContainer({ text, icon, ...props }) {
-    return (
-        <section
-            className="bg-[#e5e5e5] rounded-sm p-2 text-center cursor-pointer"
-            {...props}
-        >
-            <figure>
-                <span className="font-bold text-3xl inline-block my-4">
-                    {icon}
-                </span>
-            </figure>
-            <footer className="font-sans">{text}</footer>
-        </section>
-    )
+export default function NavContainer({
+  text,
+  icon,
+  navigateTo = '#',
+  ...props
+}) {
+  return (
+    <Link to={navigateTo}>
+      <section className="bg-[#e5e5e5] rounded-sm p-2 text-center" {...props}>
+        <figure>
+          <span className="font-bold text-3xl inline-block my-4">{icon}</span>
+        </figure>
+        <footer className="font-sans">{text}</footer>
+      </section>
+    </Link>
+  );
 }
 
 NavContainer.propTypes = {
-    text: PropTypes.string,
-    icon: PropTypes.string,
-}
+  text: PropTypes.string,
+  icon: PropTypes.string,
+  navigateTo: PropTypes.string,
+};
