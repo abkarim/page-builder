55d4f3a1db338ed8642b2027b0bf733a0f448d37
unknown <akskarim17@gmail.com>
2023-03-09 18:34:31 +0600

    new route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
-import Home from './pages/home'
+import Home from './pages/home';
+import NewPage from './pages/NewPage';
 
 export default function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/create-new-page" element={<NewPage />} />
+    </Routes>
+  );
 }
