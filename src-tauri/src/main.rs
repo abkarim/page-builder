16a2bda5412e46186c4e57cde831fada0f420cf0
unknown <akskarim17@gmail.com>
2023-03-10 05:47:06 +0600

    reading blocks from storage - resolved #2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,49 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use std::fs;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_blocks() -> String {
+    let paths = fs::read_dir("./../blocks").unwrap();
+
+    let mut content = String::from("[");
+
+    let mut currently_reading = 0;
+    for path in paths {
+        let contents = fs::read_to_string(path.unwrap().path()).expect("Failed to read file");
+
+        if currently_reading > 0 {
+            content.push_str(",");
+            content.push_str(&contents);
+        } else {
+            content.push_str(&contents);
+        }
+
+        currently_reading = currently_reading + 1;
+    }
+
+    content.push_str("]");
+    return content;
+}
+
+#[tauri::command]
+fn save_new_page(filename: &str, content: &str) -> bool {
+    return true;
+}
+
+#[tauri::command]
+fn update_page(filename: &str, content: &str) -> bool {
+    return true;
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            get_blocks,
+            save_new_page,
+            update_page
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
