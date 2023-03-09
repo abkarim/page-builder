cb65bacdd78581fe67d03ad05a591992e3b553a7
unknown <akskarim17@gmail.com>
2023-03-09 18:35:24 +0600

    editor setup updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './index.html',
-        './src/*.jsx',
-        './src/components/**/*.jsx',
-        './src/pages/*.jsx',
-    ],
-    theme: {
-        extend: {},
+  content: [
+    './index.html',
+    './src/*.jsx',
+    './src/components/**/*.jsx',
+    './src/pages/*.jsx',
+  ],
+  theme: {
+    extend: {
+      transitionProperty: {
+        width: 'width',
+        'position-left': 'left',
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
