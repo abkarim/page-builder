a76f2b2111084ebe80ec4d3bd8401ae6c09a0594
unknown <akskarim17@gmail.com>
2023-03-09 18:33:25 +0600

    Home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import NavContainer from '../components/NavContainer'
+import NavContainer from '../components/NavContainer';
 
 export default function Home() {
-    return (
-        <main className="p-2 flex gap-2">
-            <NavContainer icon="&#43;" text="Create new page" />
-        </main>
-    )
+  return (
+    <main className="p-2 flex gap-2">
+      <NavContainer
+        icon="&#43;"
+        text="Create new page"
+        navigateTo="/create-new-page"
+      />
+    </main>
+  );
 }
