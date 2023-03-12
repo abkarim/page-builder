59b479eb71622863940b32fdb802b917cef63901
unknown <akskarim17@gmail.com>
2023-03-13 02:16:05 +0600

    Removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -7,8 +6,6 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </BrowserRouter>
 );
