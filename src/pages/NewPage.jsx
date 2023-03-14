6a05d545d0782ada7aa2a2b5cd0238132e9b560a
unknown <akskarim17@gmail.com>
2023-03-14 11:00:12 +0600

    Save logic added

--- src/pages/NewPage.jsx
+++ src/pages/NewPage.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
+import { save } from '@tauri-apps/api/dialog';
+import PropType from 'prop-types';
 
 import SideBar from '../components/Sidebar';
 import Block from '../components/Block';
 import PageHeader from '../components/PageHeader';
 import AddNewBlock from '../components/AddNewBlock';
 
-const Blocks = () => {
+const Blocks = ({ setPageContent }) => {
   const [blocks, setBlocks] = useState([]);
 
   useEffect(() => {
@@ -20,35 +22,11 @@ const Blocks = () => {
   return (
     <div className="grid grid-cols-2 gap-2">
       {blocks.map((block, id) => {
-        return <Block key={id} block={block} />;
+        return <Block key={id} block={block} setPageContent={setPageContent} />;
       })}
     </div>
   );
 };
-
-function PageCanvas() {
-  const [pageData, setPageData] = useState('');
-
-  const dragOver = (e) => {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = 'copy';
-  };
-
-  const drop = (e) => {
-    setPageData((prev) => prev + e.dataTransfer.getData('html'));
-  };
-
-  return (
-    <div
-      onDragOver={dragOver}
-      onDrop={drop}
-      className="border border-t-0 border-black"
-    >
-      <div dangerouslySetInnerHTML={{ __html: pageData }} />
-      <AddNewBlock />
-    </div>
-  );
-}
 
 export default function NewPage() {
   const [pageData, setPageData] = useState({
@@ -59,17 +37,63 @@ export default function NewPage() {
       title: 'Blocks',
     },
   });
+  const [pageContent, setPageContent] = useState('');
+
+  const dragOver = (e) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+  };
+
+  const drop = (e) => {
+    setPageContent((prev) => prev + e.dataTransfer.getData('html'));
+  };
+
+  const saveData = async () => {
+    const filePath = await save({
+      filters: [
+        {
+          name: 'html',
+          extensions: ['html'],
+        },
+      ],
+    });
+
+    if (filePath === null) return;
+
+    try {
+      await invoke('save_page', {
+        filename: filePath,
+        content: pageContent,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <main className="flex justify-between">
       <SideBar title={pageData.sidebar.title}>
-        <Blocks />
+        <Blocks setPageContent={setPageContent} />
       </SideBar>
 
       <section className="w-full ml-1">
-        <PageHeader pageTitle={pageData.meta.title} />
-        <PageCanvas />
+        <PageHeader pageTitle={pageData.meta.title} onSave={saveData} />
+        <div
+          onDragOver={dragOver}
+          onDrop={drop}
+          className="border border-t-0 border-black"
+        >
+          <div
+            dangerouslySetInnerHTML={{ __html: pageContent }}
+            className="ignore"
+          />
+          <AddNewBlock />
+        </div>
       </section>
     </main>
   );
 }
+
+Blocks.propTypes = {
+  setPageContent: PropType.func,
+};
