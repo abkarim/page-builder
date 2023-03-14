df5905147ef06f51baf1356c03a9af94d9885ef9
unknown <akskarim17@gmail.com>
2023-03-14 10:59:54 +0600

    Save button added

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,13 +1,24 @@
 import PropType from 'prop-types';
 
-export default function PageHeader({ pageTitle, ...props }) {
+export default function PageHeader({ pageTitle, onSave, ...props }) {
   return (
-    <header {...props} className="bg-[#e5e5e5] p-1.5 border-b-2 border-black">
+    <header
+      className="bg-[#e5e5e5] pl-1.5 border-b-2 border-black flex justify-between items-center"
+      {...props}
+    >
       <h6 title="page title">{pageTitle}</h6>
+      <button
+        type="button"
+        onClick={onSave}
+        className="bg-blue-500 p-1.5 outline-none text-white"
+      >
+        Save
+      </button>
     </header>
   );
 }
 
 PageHeader.propTypes = {
   pageTitle: PropType.string,
+  onSave: PropType.func,
 };
