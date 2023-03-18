79a606cbfe45b7ad8917a7eac0cb6a67737d5523
unknown <akskarim17@gmail.com>
2023-03-18 16:25:49 +0600

    items are now allowed to drop into iframe - Resolved #7

--- src/util/getHTMLstructure.js
+++ src/util/getHTMLstructure.js
@@ -5,6 +5,30 @@ export default function getHTMLstructure(headerData, body) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     ${headerData}
+
+    <script id="dev-element">
+    
+      function allowDrop(e) {
+        e.preventDefault();
+      }
+      function drop(e) {
+        e.preventDefault();
+      }
+      
+      window.addEventListener('drop', function (e) {
+        window.parent.postMessage({
+          event: 'drop',
+          data: e.dataTransfer.getData('html'),
+        });
+      });
+      
+      window.addEventListener('dragover', function (e) {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'copy';
+      });
+    
+    </script>
+
   </head>
     
     <body>
