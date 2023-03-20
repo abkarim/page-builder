d84618d2de8aeb73ccca8256d29416c02610fd75
unknown <akskarim17@gmail.com>
2023-03-21 03:41:58 +0600

    Structure updated

--- src/util/getHTMLstructure.js
+++ src/util/getHTMLstructure.js
@@ -6,26 +6,93 @@ export default function getHTMLstructure(headerData, body) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     ${headerData}
 
-    <script id="dev-element">
+    <style class="page-builder-dev-element">
     
-      function allowDrop(e) {
-        e.preventDefault();
+      .siblings-of-currently-dragging-item[page-builder-element="true"]{
+        outline: 2px solid red;
+        outline-offset: 5px;
       }
-      function drop(e) {
-        e.preventDefault();
+
+      // [page-builder-element="true"]{
+      //   outline: 2px solid red;
+      //   outline-offset: 5px;
+      // }
+
+    </style>
+
+    <script class="page-builder-dev-element">
+      let target = null;
+      let before = false;
+      let draggingElement = null;
+
+      function createElementFromHTML(html) {
+        const element = document.createElement('div');
+        element.innerHTML = html;
+        return element.children[0];
       }
-      
+
       window.addEventListener('drop', function (e) {
-        window.parent.postMessage({
+        const type = e.dataTransfer.getData('type');
+      
+        const data = {
           event: 'drop',
-          data: e.dataTransfer.getData('html'),
-        });
+          data: '',
+        };
+        
+        
+        if( type ){
+          
+          let element = draggingElement;
+
+          if(!element) {
+            element = createElementFromHTML(window.parent.draggingData)
+          }
+          
+          if(type === "copy") {
+            element = element.cloneNode(true);
+          }
+          
+          if(before === true) {
+            target.parentElement.insertBefore(element, target);
+          }else {
+            if(target) {
+              target.parentElement.insertBefore(element, target.nextSibling);
+            }else {
+              document.body.appendChild(element)
+            }
+          }
+
+        }else {
+          const droppedElement = createElementFromHTML(e.dataTransfer.getData("html"));
+          document.body.appendChild(droppedElement);
+        }
+
+        data.data = document.body.innerHTML.toString().trim();
+        window.parent.postMessage(data);
+        target = null;
+        draggingElement = null;
       });
       
       window.addEventListener('dragover', function (e) {
         e.preventDefault();
-        e.dataTransfer.dropEffect = 'copy';
+        
+        if(e.target?.hasAttribute("page-builder-element")) {
+          target = e.target;
+          const {width, height} = target.getBoundingClientRect();
+          const {clientX, clientY} = e;
+          before = (width > clientX && height > clientY);
+        }else {
+          target = null;
+        }
+
       });
+
+      function dragStart(e) {
+        e.dataTransfer.effectAllowed = "copyMove";
+        e.dataTransfer.dropEffect = "move";
+        e.dataTransfer.setData("type", "move");
+        draggingElement = e.target;
+      }
     
     </script>
 
