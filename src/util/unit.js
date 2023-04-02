509b616b14aa1e6a8cdaada31750aa4dc4bf3ab3
unknown <akskarim17@gmail.com>
2023-04-02 23:03:30 +0600

    UNIT added

---
+++ src/util/unit.js
@@ -0,0 +1,18 @@
+export default function Unit() {
+  return {
+    '%': '%',
+    PX: 'px',
+    EM: 'em',
+    REM: 'rem',
+    VW: 'vw',
+    VH: 'vh',
+    VMIN: 'vmin',
+    VMAX: 'vmax',
+    CH: 'ch',
+    CM: 'cm',
+    MM: 'mm',
+    IN: 'in',
+    PT: 'pt',
+    PC: 'pc',
+  };
+}
