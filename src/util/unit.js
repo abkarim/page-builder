b342dec65deea0a253ce9e67d44cfcabb49178c1
unknown <akskarim17@gmail.com>
2023-04-10 03:27:36 +0600

    Returns units array

--- src/util/unit.js
+++ src/util/unit.js
@@ -1,18 +1,18 @@
-export default function Unit() {
-  return {
-    '%': '%',
-    PX: 'px',
-    EM: 'em',
-    REM: 'rem',
-    VW: 'vw',
-    VH: 'vh',
-    VMIN: 'vmin',
-    VMAX: 'vmax',
-    CH: 'ch',
-    CM: 'cm',
-    MM: 'mm',
-    IN: 'in',
-    PT: 'pt',
-    PC: 'pc',
-  };
+export default function unit() {
+  return [
+    { name: '%', value: '%' },
+    { name: 'PX', value: 'px' },
+    { name: 'EM', value: 'em' },
+    { name: 'REM', value: 'rem' },
+    { name: 'VW', value: 'vw' },
+    { name: 'VH', value: 'vh' },
+    { name: 'VMIN', value: 'vmin' },
+    { name: 'VMAX', value: 'vmax' },
+    { name: 'CH', value: 'ch' },
+    { name: 'CM', value: 'cm' },
+    { name: 'MM', value: 'mm' },
+    { name: 'IN', value: 'in' },
+    { name: 'PT', value: 'pt' },
+    { name: 'PC', value: 'pc' },
+  ];
 }
