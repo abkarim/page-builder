01e27633d465d3c88484dd70b2e495e122093639
unknown <akskarim17@gmail.com>
2023-04-10 06:31:20 +0600

    Wrapper for style content

---
+++ src/components/StyleEditor.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import PropType from 'prop-types';
+
+export default function StyleEditor({ title, children, ...props }) {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggleIsVisible = () => {
+    setIsVisible((prev) => !prev);
+  };
+
+  return (
+    <div {...props}>
+      <div
+        title="toggle visibility"
+        className="flex justify-between items-center cursor-pointer"
+        onClick={toggleIsVisible}
+      >
+        <h2 className="text-lg">{title}</h2>
+        <div className="bg-white rounded-full p-1 flex justify-center items-center w-5 h-5 font-bold select-none">
+          <span
+            className={`inline-block ${
+              isVisible ? '-rotate-90' : '!rotate-180'
+            } `}
+          >
+            &#10094;
+          </span>
+        </div>
+      </div>
+      <section>{isVisible && children}</section>
+    </div>
+  );
+}
+
+StyleEditor.propTypes = {
+  title: PropType.string,
+  children: PropType.element,
+};
