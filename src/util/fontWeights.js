c124ef3b660d545461c2ab3fd618fd7f9fa8b1b5
unknown <akskarim17@gmail.com>
2023-04-11 20:36:49 +0600

    Utility functions added

---
+++ src/util/fontWeights.js
@@ -0,0 +1,13 @@
+export default function fontWeights() {
+  return [
+    { name: 'Thin', value: 100 },
+    { name: 'Extra Light', value: 200 },
+    { name: 'Light', value: 300 },
+    { name: 'Normal', value: 400 },
+    { name: 'Medium', value: 500 },
+    { name: 'Semi Bold', value: 600 },
+    { name: 'Bold', value: 700 },
+    { name: 'Extra Bold', value: 800 },
+    { name: 'Heavy', value: 900 },
+  ];
+}
---
+++ src/util/fonts.js
@@ -0,0 +1,13 @@
+export default function fonts() {
+  return [
+    { name: 'Arial', value: 'Arial, sans-serif' },
+    { name: 'Verdana', value: 'Verdana, sans-serif' },
+    { name: 'Tahoma', value: 'Tahoma, sans-serif' },
+    { name: 'Trebuchet MS', value: "'Trebuchet MS', sans-serif" },
+    { name: 'Times New Roman', value: "'Times New Roman', serif" },
+    { name: 'Georgia', value: 'Georgia, serif' },
+    { name: 'Garamond', value: 'Garamond, serif' },
+    { name: 'Courier New', value: "'Courier New', monospace" },
+    { name: 'Brush Script MT ', value: "'Brush Script MT', cursive" },
+  ];
+}
