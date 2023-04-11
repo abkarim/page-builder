0eb6f4015aac499970c9186f79462b2f3eb39ac6
unknown <akskarim17@gmail.com>
2023-04-11 18:29:02 +0600

    Color init

--- src/components/stylesEditor/Color.jsx
+++ src/components/stylesEditor/Color.jsx
@@ -1,3 +1,73 @@
-export default function Color() {
-  return <h1>Color</h1>;
+/* eslint-disable no-prototype-builtins */
+import { useCallback, useEffect, useState } from 'react';
+import PropType from 'prop-types';
+
+import ColorInput from '../input/Color';
+import Wrapper from './Wrapper';
+
+export default function Color({ prevData, setStyle }) {
+  const [data, setData] = useState({
+    color: '#000000',
+    backgroundColor: '#FFFFFF',
+  });
+
+  // * Prepare data from prevData
+  useEffect(() => {
+    //! prevData contains full style objects like color, outline, border
+    // extract only required info
+    if (prevData.hasOwnProperty('color')) {
+      let prev = prevData.color;
+      setData((pre) => {
+        return { ...pre, ...prev };
+      });
+    }
+  }, []);
+
+  // * Updates value in main data
+  useEffect(() => {
+    // Prepare final value
+    const finalData = data;
+    finalData.final = `color: ${data.color};`;
+    finalData.final += `background-color: ${data.backgroundColor};`;
+
+    setStyle((prev) => {
+      return { ...prev, color: finalData };
+    });
+  }, [data]);
+
+  const updateColor = useCallback((target, color) => {
+    setData((prev) => {
+      return {
+        ...prev,
+        [target]: color,
+      };
+    });
+  });
+
+  // eslint-disable-next-line react/prop-types
+  const Input = ({ title, target }) => {
+    return (
+      <div className="flex justify-between">
+        <label>{title}</label>
+        <ColorInput
+          value={data[target]}
+          onChange={(e) => updateColor(target, e.target.value)}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <Wrapper title="Color">
+      <div className="space-y-1">
+        <Input target="color" title="Color" />
+        <Input target="backgroundColor" title="Background Color" />
+      </div>
+    </Wrapper>
+  );
 }
+
+Color.propTypes = {
+  prevData: PropType.object,
+  setStyle: PropType.func,
+};
