0a1fc4c0ea801e549f8dcaa982062e7cf92bb484
unknown <akskarim17@gmail.com>
2023-04-17 02:16:36 +0600

    Refactored

---
+++ src/components/Editors.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import ElementEditors from './elementEditor/ElementEditors';
+import StyleEditors from './stylesEditor/StyleEditors';
+
+export default function Editors({ ...props }) {
+  const [editor, setEditor] = useState('style');
+  return (
+    <div>
+      <header className="flex justify-evenly gap-1">
+        <button
+          className={`bg-white w-full ${editor === 'style' && 'outline'}`}
+          onClick={() => setEditor('style')}
+        >
+          style
+        </button>
+        <button
+          className={`bg-white w-full ${editor === 'element' && 'outline'}`}
+          onClick={() => setEditor('element')}
+        >
+          element
+        </button>
+      </header>
+      <div className={`${editor === 'element' && 'hidden'}`}>
+        <StyleEditors {...props} />
+      </div>
+      <div className={`${editor === 'style' && 'hidden'}`}>
+        <ElementEditors {...props} />
+      </div>
+    </div>
+  );
+}
