302325f81afbbd00015e214ac96ece37e8b6acbb
karim <akskarim17@gmail.com>
2025-11-01 17:34:47 +0600

    Added directory selector

--- src/screens/project/Create.tsx
+++ src/screens/project/Create.tsx
@@ -1,3 +1,80 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Spinner } from "@/components/ui/spinner";
+import { Label } from "@radix-ui/react-context-menu";
+import { useState } from "react";
+import { toast } from "sonner";
+import { invoke } from "@tauri-apps/api/core";
+import { open } from "@tauri-apps/plugin-dialog";
+
 export default function (): React.JSX.Element {
-    return <section>Create new project</section>;
+    const [name, setName] = useState("");
+    const [directory, setDirectory] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    async function selectDirectory() {
+        try {
+            /**
+             * Select folder path
+             */
+            const selectedFolder = await open({
+                multiple: false,
+                directory: true,
+            });
+
+            setDirectory(selectedFolder);
+        } catch (e) {
+            console.error(e);
+            toast.error("something went wrong");
+        }
+    }
+
+    async function create() {
+        if (name.trim().length === 0) {
+            return toast.error("Name can't be empty");
+        }
+
+        setIsLoading(true);
+
+        try {
+            const data = await invoke("create_project", {
+                name,
+            });
+            console.log(data);
+        } catch (e) {
+            toast.error("Something went wrong, please try again");
+            console.error(e);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return (
+        <section>
+            <fieldset>
+                <Label>Select Project Directory</Label>
+                <div className="flex items-center justify-start gap-2">
+                    <Button onClick={selectDirectory} variant="secondary">
+                        {directory === null ? "Select" : "Change"} directory
+                    </Button>
+                    {directory !== null && <p>{directory}</p>}
+                </div>
+            </fieldset>
+            <fieldset>
+                <Label>Project name</Label>
+                <Input
+                    type="text"
+                    placeholder="project name"
+                    className="w-full"
+                    value={name}
+                    onInput={(e) => setName(e.currentTarget.value)}
+                />
+            </fieldset>
+            <div className="mt-3">
+                <Button disabled={isLoading} onClick={create}>
+                    {isLoading && <Spinner />} Create
+                </Button>
+            </div>
+        </section>
+    );
 }
