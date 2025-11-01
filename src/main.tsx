19bc0acbf4ae57047ee894f6e47fc835fae4835a
karim <akskarim17@gmail.com>
2025-11-01 16:42:45 +0600

    toast added

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { SidebarProvider } from "./components/ui/sidebar";
+import { Toaster } from "./components/ui/sonner";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
             <SidebarProvider>
                 <App />
+                <Toaster />
             </SidebarProvider>
         </BrowserRouter>
     </React.StrictMode>
