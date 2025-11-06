c5240be0bb873651e7059f9246c4356d2f9e0cb8
karim <akskarim17@gmail.com>
2025-11-06 16:17:50 +0600

    updated modules

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,8 @@
 mod commands;
+mod db;
+mod fs;
+
+use commands::projects;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -6,7 +10,10 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![commands::projects::create_project,])
+        .invoke_handler(tauri::generate_handler![
+            projects::create_project,
+            projects::get_projects
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
