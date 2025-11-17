04dcc65f8319a71c2b52cd22ddaab67bac53c8b5
karim <akskarim17@gmail.com>
2025-11-17 20:46:24 +0600

    getting project data

--- src/screens/project/View.tsx
+++ src/screens/project/View.tsx
@@ -1,3 +1,32 @@
+import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "sonner";
+import { type Project } from "src-tauri/bindings/Project";
+
 export default function (): React.JSX.Element {
+    const { id } = useParams();
+    const [project, setProject] = useState<Project>();
+
+    async function getProject() {
+        try {
+            const data = await invoke<Project>("get_project", {
+                uuid: id,
+            });
+
+            console.log({ data });
+        } catch (err) {
+            toast.error(err as string);
+        }
+    }
+
+    useEffect(() => {
+        if (!id) {
+            toast.error("Project is not valid, invalid uuid");
+            return;
+        }
+        getProject();
+    }, []);
+
     return <section>View</section>;
 }
