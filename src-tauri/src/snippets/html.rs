8980062492bce311a437289cd22a5dfe8c6d2385
karim <akskarim17@gmail.com>
2025-11-18 22:19:43 +0600

    added default css and js

--- src-tauri/src/snippets/html.rs
+++ src-tauri/src/snippets/html.rs
@@ -1,15 +1,25 @@
+use crate::commands::projects;
+
 pub fn get_html_snippet(title: &String) -> String {
     format!(
         r#"
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>{}</title>
-        </head>
-        <body>
-        </body>
-        </html>
-    "#,
-        title
+            <!DOCTYPE html>
+            <html lang="en">
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>{title}</title>
+                <link rel="stylesheet" href="{css_path}styles.css">
+            </head>
+            <body>
+
+                <script src"{js_path}app.js"></script>
+            </body>
+            </html>
+        "#,
+        title = title,
+        css_path = projects::ASSETS_CSS_PATH,
+        js_path = projects::ASSETS_JS_PATH
     )
 }
