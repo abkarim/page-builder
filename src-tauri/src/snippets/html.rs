dd2329fa8d773c9f54229fc58fad14f0ae98c2dd
karim <akskarim17@gmail.com>
2025-11-19 18:09:04 +0600

    updated strcuture

--- src-tauri/src/snippets/html.rs
+++ src-tauri/src/snippets/html.rs
@@ -10,16 +10,18 @@ pub fn get_html_snippet(title: &String) -> String {
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>{title}</title>
-                <link rel="stylesheet" href="{css_path}styles.css">
+                <link rel="stylesheet" href="/{css_path}{project_css_filename}">
             </head>
             <body>
 
-                <script src"{js_path}app.js"></script>
+                <script src="/{js_path}{project_js_filename}"></script>
             </body>
             </html>
         "#,
         title = title,
-        css_path = projects::ASSETS_CSS_PATH,
-        js_path = projects::ASSETS_JS_PATH
+        css_path = projects::PROJECT_ASSETS_CSS_PATH,
+        js_path = projects::PROJECT_ASSETS_JS_PATH,
+        project_css_filename = projects::PROJECT_CSS_FILENAME,
+        project_js_filename = projects::PROJECT_JS_FILENAME
     )
 }
