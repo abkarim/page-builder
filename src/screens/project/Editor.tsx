da3c365d561a1d415c0a95b9dda91feb56093c1b
karim <akskarim17@gmail.com>
2025-11-24 20:24:54 +0600

    getting content

--- src/screens/project/Editor.tsx
+++ src/screens/project/Editor.tsx
@@ -1,19 +1,34 @@
-import { useEffect } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 
 export default function Editor() {
     const { id, name } = useParams();
+    const [content, setContent] = useState("");
 
     async function getContent() {
         if (!id || !name) {
             return toast.error("invalid project file");
+        }
+
+        try {
+            const content = await invoke<string>("get_project_file_content", {
+                uuid: id,
+                name,
+            });
+
+            setContent(content);
+        } catch (err) {
+            toast.error(err as string);
         }
     }
 
     useEffect(() => {
         getContent();
     }, []);
+
+    console.log({ content });
 
     return (
         <section>
