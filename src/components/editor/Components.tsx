4eff8ff5508fc9710243439971391450c9b1576a
karim <akskarim17@gmail.com>
2025-12-04 19:42:59 +0600

    getting blocks

--- src/components/editor/Components.tsx
+++ src/components/editor/Components.tsx
@@ -1,17 +1,37 @@
+import { useEffect, useState } from "react";
 import {
     Sidebar,
     SidebarContent,
     SidebarProvider,
     SidebarTrigger,
 } from "../ui/sidebar";
+import { toast } from "sonner";
+import { invoke } from "@tauri-apps/api/core";
 
-export default function Elements(): React.JSX.Element {
+export default function Components(): React.JSX.Element {
+    const [blocks, setBlocks] = useState([]);
+
+    async function get_blocks() {
+        try {
+            const data = await invoke<string>("get_blocks");
+            setBlocks(JSON.parse(data));
+        } catch (err) {
+            toast.error(err as string);
+        }
+    }
+
+    useEffect(() => {
+        get_blocks();
+    }, []);
+
+    console.log({ blocks });
+
     return (
         <section>
             <SidebarProvider defaultOpen={true} className="flex flex-col">
                 <div className="flex items-center gap-1">
                     <SidebarTrigger className="z-10" />
-                    <h4>Elements</h4>
+                    <h4>Components</h4>
                 </div>
                 <Sidebar
                     variant="sidebar"
@@ -19,7 +39,7 @@ export default function Elements(): React.JSX.Element {
                     className="relative z-0"
                 >
                     <SidebarContent>
-                        <section>Elements</section>
+                        <section>components</section>
                     </SidebarContent>
                 </Sidebar>
             </SidebarProvider>
