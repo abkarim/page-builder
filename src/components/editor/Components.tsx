a4874c16d6867ae33f3f4b91a3cbb7a7b87b55bc
karim <akskarim17@gmail.com>
2025-12-07 19:44:37 +0600

    getting components

--- src/components/editor/Components.tsx
+++ src/components/editor/Components.tsx
@@ -9,9 +9,12 @@ import { toast } from "sonner";
 import { invoke } from "@tauri-apps/api/core";
 import { Blocks } from "./blocks";
 import { Button } from "../ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import { Component } from "./components";
 
 export default function Components(): React.JSX.Element {
     const [blocks, setBlocks] = useState<Blocks[]>([]);
+    const [components, setComponents] = useState<Component[]>([]);
 
     async function get_blocks() {
         try {
@@ -22,8 +25,18 @@ export default function Components(): React.JSX.Element {
         }
     }
 
+    async function get_components() {
+        try {
+            const data = await invoke<string>("get_components");
+            setComponents(JSON.parse(data));
+        } catch (err) {
+            toast.error(err as string);
+        }
+    }
+
     useEffect(() => {
         get_blocks();
+        get_components();
     }, []);
 
     return (
@@ -39,11 +52,42 @@ export default function Components(): React.JSX.Element {
                     className="relative z-0"
                 >
                     <SidebarContent>
-                        <section className="flex flex-wrap gap-1 p-2">
-                            {blocks.map((block) => (
-                                <Button key={block.id}>{block.name}</Button>
-                            ))}
-                        </section>
+                        <Tabs defaultValue="blocks" className="p-2">
+                            <TabsList>
+                                <TabsTrigger value="blocks">Blocks</TabsTrigger>
+                                <TabsTrigger value="components">
+                                    Components
+                                </TabsTrigger>
+                            </TabsList>
+                            <TabsContent value="components">
+                                <section className="flex flex-wrap gap-1">
+                                    <>
+                                        {components.map((component) => (
+                                            <Button key={component.id}>
+                                                {component.name}
+                                            </Button>
+                                        ))}
+                                        {components.length === 0 && (
+                                            <p>No results</p>
+                                        )}
+                                    </>
+                                </section>
+                            </TabsContent>
+                            <TabsContent value="blocks">
+                                <section className="flex flex-wrap gap-1">
+                                    <>
+                                        {blocks.map((block) => (
+                                            <Button key={block.id}>
+                                                {block.name}
+                                            </Button>
+                                        ))}
+                                        {blocks.length === 0 && (
+                                            <p>No results</p>
+                                        )}
+                                    </>
+                                </section>
+                            </TabsContent>
+                        </Tabs>
                     </SidebarContent>
                 </Sidebar>
             </SidebarProvider>
