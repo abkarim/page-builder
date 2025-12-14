524ea8b0d1a91ddce084c43017983fd133b4a267
karim <akskarim17@gmail.com>
2025-12-14 18:12:11 +0600

    fix error

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,7 +10,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             projects::create_project,
             projects::remove_project,
