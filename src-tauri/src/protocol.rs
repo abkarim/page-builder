472799a160bd05a4c4d6045df28396a69a9eb01d
karim <akskarim17@gmail.com>
2025-12-15 19:59:21 +0600

    init getting project path

--- src-tauri/src/protocol.rs
+++ src-tauri/src/protocol.rs
@@ -3,12 +3,25 @@ use tauri::{
     AppHandle,
 };
 
+use crate::commands::projects;
+
 pub fn register_custom_protocol(
     app_handle: &AppHandle,
     request: &Request<Vec<u8>>,
 ) -> Response<Vec<u8>> {
     let uri = request.uri().to_string();
     println!("{:?}", uri);
+
+    let project_root = match projects::get_project_root() {
+        Ok(path) => path,
+        Err(err) => {
+            println!("{}", err);
+            return Response::builder()
+                .status(404)
+                .body("Not found".as_bytes().to_vec())
+                .unwrap();
+        }
+    };
 
     let response_body = format!("Hello from Tauri! You requested: {}", uri);
 
