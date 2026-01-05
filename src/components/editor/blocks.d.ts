9a3417ab8772194e03c675cbb8c0882f8b5ea808
karim <akskarim17@gmail.com>
2026-01-05 18:35:34 +0600

    changed name

--- src/components/editor/blocks.d.ts
+++ src/components/editor/blocks.d.ts
@@ -1,9 +1,7 @@
 export type Blocks = {
-    id: number;
-    name: string;
-    element: string;
-    default_content?: string;
-    available_styles: string[];
-    available_edits: string[];
-    alternative_tags?: string[];
+  id: number;
+  name: string;
+  tag: string;
+  content?: string;
+  attributes?: [Record<string, string>];
 };
