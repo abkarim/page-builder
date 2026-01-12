8c9fc3bf72e7a19f1847bac67194471e1a823719
karim <akskarim17@gmail.com>
2026-01-12 18:37:50 +0600

    init color picker

---
+++ src/components/ColorPicker.tsx
@@ -0,0 +1,38 @@
+import {
+  ColorPicker,
+  ColorPickerAlphaSlider,
+  ColorPickerArea,
+  ColorPickerContent,
+  ColorPickerEyeDropper,
+  ColorPickerFormatSelect,
+  ColorPickerHueSlider,
+  ColorPickerInput,
+  ColorPickerSwatch,
+  ColorPickerTrigger,
+} from "@/components/ui/color-picker";
+
+export default function ColorPickerComponent(): React.JSX.Element {
+  return (
+    <div>
+      <ColorPicker defaultFormat="rgb" defaultValue="#3b82f6">
+        <ColorPickerTrigger asChild>
+          <ColorPickerSwatch />
+        </ColorPickerTrigger>
+        <ColorPickerContent>
+          <ColorPickerArea />
+          <div className="flex items-center gap-2">
+            <ColorPickerEyeDropper />
+            <div className="flex flex-1 flex-col gap-2">
+              <ColorPickerHueSlider />
+              <ColorPickerAlphaSlider />
+            </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <ColorPickerFormatSelect />
+            <ColorPickerInput />
+          </div>
+        </ColorPickerContent>
+      </ColorPicker>
+    </div>
+  );
+}
