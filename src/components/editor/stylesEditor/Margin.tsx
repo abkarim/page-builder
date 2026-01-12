cd38d2befe3e348ff1f667e941b3858afebe0df0
karim <akskarim17@gmail.com>
2026-01-12 16:55:51 +0600

    updated font size

--- src/components/editor/stylesEditor/Margin.tsx
+++ src/components/editor/stylesEditor/Margin.tsx
@@ -1,5 +1,10 @@
 import CombinedDetachedInput from "./CombinedDetachedInput";
 
 export default function Margin() {
-  return <CombinedDetachedInput />;
+  return (
+    <div className="space-y-1">
+      <h6 className="text-sm">Margin</h6>
+      <CombinedDetachedInput />
+    </div>
+  );
 }
--- src/components/editor/stylesEditor/Padding.tsx
+++ src/components/editor/stylesEditor/Padding.tsx
@@ -1,5 +1,10 @@
 import CombinedDetachedInput from "./CombinedDetachedInput";
 
 export default function Padding() {
-  return <CombinedDetachedInput />;
+  return (
+    <div className="space-y-1">
+      <h6 className="text-sm">Padding</h6>
+      <CombinedDetachedInput />
+    </div>
+  );
 }
