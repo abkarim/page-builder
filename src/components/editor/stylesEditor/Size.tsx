77d7cf21dbe43392af4dbffc335d1f37f7731b2c
karim <akskarim17@gmail.com>
2026-01-15 17:50:34 +0600

    toggle advance inputs

--- src/components/editor/stylesEditor/Size.tsx
+++ src/components/editor/stylesEditor/Size.tsx
@@ -1,7 +1,11 @@
+import { Checkbox } from "@/components/ui/checkbox";
 import CSSValueInput from "@/components/ui/CSSValueInput";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
 export default function Size() {
+  const [showAdvanced, setShowAdvanced] = useState(false);
+
   return (
     <div className="space-y-1">
       <h6 className="text-sm">Size</h6>
@@ -13,22 +17,36 @@ export default function Size() {
         <Label className="text-sm">Height</Label>
         <CSSValueInput onChange={() => {}} />
       </div>
-      <div>
-        <Label className="text-sm">Min Width</Label>
-        <CSSValueInput onChange={() => {}} />
+      <div className="flex items-center gap-1 justify-start">
+        <Label htmlFor="show-advanced-size" className="text-xs">
+          Show advanced
+        </Label>
+        <Checkbox
+          id="show-advanced-size"
+          checked={showAdvanced}
+          onCheckedChange={() => setShowAdvanced((prev) => !prev)}
+        />
       </div>
-      <div>
-        <Label className="text-sm">Min Height</Label>
-        <CSSValueInput onChange={() => {}} />
-      </div>
-      <div>
-        <Label className="text-sm">Max Width</Label>
-        <CSSValueInput onChange={() => {}} />
-      </div>
-      <div>
-        <Label className="text-sm">Max Height</Label>
-        <CSSValueInput onChange={() => {}} />
-      </div>
+      {showAdvanced && (
+        <>
+          <div>
+            <Label className="text-sm">Min Width</Label>
+            <CSSValueInput onChange={() => {}} />
+          </div>
+          <div>
+            <Label className="text-sm">Min Height</Label>
+            <CSSValueInput onChange={() => {}} />
+          </div>
+          <div>
+            <Label className="text-sm">Max Width</Label>
+            <CSSValueInput onChange={() => {}} />
+          </div>
+          <div>
+            <Label className="text-sm">Max Height</Label>
+            <CSSValueInput onChange={() => {}} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
