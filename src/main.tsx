6084566c5c034e2555ff6a047e77ba05461ddc27
karim <akskarim17@gmail.com>
2026-02-02 18:31:39 +0600

    removed strict mode to only load once

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { SidebarProvider } from "./components/ui/sidebar";
@@ -7,12 +6,10 @@ import { ConfirmDialogContextProvider } from "./context/ConfirmDialogContext";
 import { router } from "./screens/router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <ConfirmDialogContextProvider>
-            <SidebarProvider>
-                <RouterProvider router={router} />
-                <Toaster />
-            </SidebarProvider>
-        </ConfirmDialogContextProvider>
-    </React.StrictMode>
+  <ConfirmDialogContextProvider>
+    <SidebarProvider>
+      <RouterProvider router={router} />
+      <Toaster />
+    </SidebarProvider>
+  </ConfirmDialogContextProvider>,
 );
