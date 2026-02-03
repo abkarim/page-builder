0d937306d8fe8daafac75d94aceb0689248b3e52
karim <akskarim17@gmail.com>
2026-02-03 21:25:32 +0600

    getting and setting spacing

--- src/components/editor/stylesEditor/Spacing.tsx
+++ src/components/editor/stylesEditor/Spacing.tsx
@@ -6,17 +6,37 @@ export interface SpacingData {
   padding: string;
 }
 
-export default function Spacing() {
+export default function Spacing({
+  data,
+  updateStyle,
+}: {
+  data: SpacingData;
+  updateStyle: (data: Partial<SpacingData>) => void;
+}) {
   return (
     <div className="space-y-1">
       <h6 className="text-sm">Spacing</h6>
       <div>
         <Label className="text-sm">Margin</Label>
-        <CombinedDetachedInput />
+        <CombinedDetachedInput
+          value={data.margin}
+          onUpdate={(value) =>
+            updateStyle({
+              margin: value,
+            })
+          }
+        />
       </div>
       <div>
         <Label className="text-sm">Padding</Label>
-        <CombinedDetachedInput />
+        <CombinedDetachedInput
+          value={data.padding}
+          onUpdate={(value) =>
+            updateStyle({
+              padding: value,
+            })
+          }
+        />
       </div>
     </div>
   );
