9af09eac4c6f855a5f97631c3b163c10c07979f8
karim <akskarim17@gmail.com>
2026-02-08 19:59:49 +0600

    added func to get xpath

--- editor-assets/js/util.ts
+++ editor-assets/js/util.ts
@@ -1,3 +1,39 @@
+/**
+ * Generates a unique XPath for a given DOM element.
+ * @param el The target DOM element.
+ * @returns The XPath string.
+ */
+export function getXPath(el: Element | null): string {
+  if (!el) {
+    return "";
+  }
+  const parts: string[] = [];
+  let currentNode: Element | null = el;
+
+  while (currentNode && currentNode.nodeType === Node.ELEMENT_NODE) {
+    let part = currentNode.tagName.toLowerCase();
+    let siblings = currentNode.parentNode?.children;
+
+    if (siblings && siblings.length > 1) {
+      let count = 1;
+      for (let i = 0; i < siblings.length; i++) {
+        const sibling = siblings[i];
+        if (sibling.nodeName === currentNode.nodeName) {
+          if (sibling === currentNode) {
+            part += `[${count}]`;
+            break;
+          }
+          count++;
+        }
+      }
+    }
+    parts.unshift(part);
+    currentNode = currentNode.parentNode as Element | null;
+  }
+
+  return `/${parts.join("/")}`;
+}
+
 export function throttle(func: Function, limit: number) {
   let inThrottle: boolean;
   return function (this: any, ...args: any[]) {
