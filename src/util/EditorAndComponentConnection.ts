1a4f3f813a36f82d0ab3a669bd89294b19264a65
Karim <akskarim17@gmail.com>
2026-02-14 19:42:11 +0600

    init element editor connections

--- src/util/EditorAndComponentConnection.ts
+++ src/util/EditorAndComponentConnection.ts
@@ -54,3 +54,50 @@ export function isEditorAvailableForComponent(
 
   return connection.availableFor.includes(component);
 }
+
+const editorConnections: {
+  editorName: string;
+  availableFor: string[] | "*";
+  notAvailableFor?: string[];
+}[] = [
+  {
+    editorName: "text",
+    availableFor: ["p", "h1", "h2", "h3", "h5", "h6"],
+  },
+  {
+    editorName: "heading",
+    availableFor: ["h1", "h2", "h3", "h4", "h5", "h6"],
+  },
+  {
+    editorName: "image",
+    availableFor: ["img"],
+  },
+  {
+    editorName: "button",
+    availableFor: ["button"],
+  },
+  {
+    editorName: "tag",
+    availableFor: ["div", "section", "column"],
+  },
+];
+
+export function isElementEditorAvilableForComponent(
+  editor: string,
+  component: string,
+) {
+  const connection = editorConnections.find(
+    (conn) => conn.editorName === editor,
+  );
+  if (!connection) return false;
+
+  /**
+   * Not available if this is
+   * explicitly disabled for this component
+   */
+  if (connection.notAvailableFor?.includes(component)) return false;
+
+  if (connection.availableFor === "*") return true;
+
+  return connection.availableFor.includes(component);
+}
