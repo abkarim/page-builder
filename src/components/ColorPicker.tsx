71c697e549ac119a6f683c4b274cc5a3a55b3b19
Karim <akskarim17@gmail.com>
2026-04-06 20:01:45 +0600

    showing project specific colors

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,68 +1,109 @@
 import {
-  ColorPicker,
-  ColorPickerAlphaSlider,
-  ColorPickerArea,
-  ColorPickerContent,
-  ColorPickerEyeDropper,
-  ColorPickerFormatSelect,
-  ColorPickerHueSlider,
-  ColorPickerInput,
-  ColorPickerSwatch,
-  ColorPickerTrigger,
+    ColorPicker,
+    ColorPickerAlphaSlider,
+    ColorPickerArea,
+    ColorPickerContent,
+    ColorPickerEyeDropper,
+    ColorPickerFormatSelect,
+    ColorPickerHueSlider,
+    ColorPickerInput,
+    ColorPickerSwatch,
+    ColorPickerTrigger,
 } from "@/components/ui/color-picker";
 import { useIsFirstRender } from "@uidotdev/usehooks";
 import { useEffect, useRef, useState } from "react";
+import { ProjectConfiguration } from "src-tauri/bindings/ProjectConfiguration";
+import { getProjectDataConfiguration } from "@/util/projectSpecific/projectData";
+import { Label } from "./ui/label";
 
 interface Props {
-  defaultValue?: string;
-  value?: string;
-  onValueChange: (color: string) => void;
+    defaultValue?: string;
+    value?: string;
+    onValueChange: (color: string) => void;
+    showAvailableColors?: boolean;
 }
 
 export default function ColorPickerComponent({
-  defaultValue,
-  value,
-  onValueChange,
+    defaultValue,
+    value,
+    onValueChange,
+    showAvailableColors = true,
 }: Props): React.JSX.Element {
-  const isFirstRender = useIsFirstRender();
-  const [color, setColor] = useState(value || defaultValue);
-  const tracker = useRef(false);
+    const isFirstRender = useIsFirstRender();
+    const [color, setColor] = useState(value || defaultValue);
+    const tracker = useRef(false);
+    const [availableColors, setAvailableColors] = useState<
+        ProjectConfiguration["color"]
+    >([]);
 
-  useEffect(() => {
-    if (!onValueChange || !color || isFirstRender || tracker.current === false)
-      return;
-    onValueChange(color);
-    tracker.current = false;
-  }, [color, isFirstRender]);
+    async function getAvailableColors() {
+        const data = await getProjectDataConfiguration();
 
-  return (
-    <div>
-      <ColorPicker
-        defaultFormat="rgb"
-        defaultValue={color}
-        onValueChange={(color) => {
-          tracker.current = true;
-          setColor(color);
-        }}
-      >
-        <ColorPickerTrigger asChild>
-          <ColorPickerSwatch color={color} />
-        </ColorPickerTrigger>
-        <ColorPickerContent>
-          <ColorPickerArea />
-          <div className="flex items-center gap-2">
-            <ColorPickerEyeDropper />
-            <div className="flex flex-1 flex-col gap-2">
-              <ColorPickerHueSlider />
-              <ColorPickerAlphaSlider />
-            </div>
-          </div>
-          <div className="flex items-center gap-2">
-            <ColorPickerFormatSelect />
-            <ColorPickerInput />
-          </div>
-        </ColorPickerContent>
-      </ColorPicker>
-    </div>
-  );
+        setAvailableColors(data.configuration.color);
+    }
+
+    useEffect(() => {
+        if (!showAvailableColors) return;
+
+        getAvailableColors();
+    }, [showAvailableColors]);
+
+    useEffect(() => {
+        if (
+            !onValueChange ||
+            !color ||
+            isFirstRender ||
+            tracker.current === false
+        )
+            return;
+        onValueChange(color);
+        tracker.current = false;
+    }, [color, isFirstRender]);
+
+    return (
+        <ColorPicker
+            defaultFormat="rgb"
+            defaultValue={color}
+            onValueChange={(color) => {
+                tracker.current = true;
+                setColor(color);
+            }}
+        >
+            <ColorPickerTrigger asChild>
+                <ColorPickerSwatch color={color} />
+            </ColorPickerTrigger>
+            <ColorPickerContent>
+                <ColorPickerArea />
+                <div className="flex items-center gap-2">
+                    <ColorPickerEyeDropper />
+                    <div className="flex flex-1 flex-col gap-2">
+                        <ColorPickerHueSlider />
+                        <ColorPickerAlphaSlider />
+                    </div>
+                </div>
+                <div className="flex items-center gap-2">
+                    <ColorPickerFormatSelect />
+                    <ColorPickerInput />
+                </div>
+                {showAvailableColors && (
+                    <section className="space-y-2">
+                        <Label>Project colors</Label>
+                        <div className="flex items-center gap-2">
+                            {availableColors.map((color, index) => (
+                                <ColorPickerSwatch
+                                    key={index}
+                                    title={color.name}
+                                    color={color.value}
+                                    onClick={() => {
+                                        tracker.current = true;
+                                        setColor(color.value);
+                                    }}
+                                />
+                            ))}
+                        </div>
+                    </section>
+                )}
+            </ColorPickerContent>
+        </ColorPicker>
+    );
 }
