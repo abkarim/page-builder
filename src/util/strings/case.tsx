e6d9d1930a5ca4a007e302d2dd3367ac03f6ae25
Karim <akskarim17@gmail.com>
2026-04-09 19:44:17 +0600

    added capitalize function

--- src/util/strings/case.tsx
+++ src/util/strings/case.tsx
@@ -4,3 +4,7 @@ export const toPascalCase = (str: string) => {
         .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
         .join("");
 };
+
+export function capitalize(str: string) {
+    return `${str.slice(0, 1).toUpperCase()}${str.slice(1)}`;
+}
